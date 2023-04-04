09047436282f34257d8f89db4c1995049bb87e9d
Ilya <2batyanya@gmail.com>
2023-04-04 19:02:23 +0300

    Methods to instantiate fragments

--- app/src/main/java/com/example/fragments/ImageFragment.kt
+++ app/src/main/java/com/example/fragments/ImageFragment.kt
@@ -7,27 +7,44 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 
+const val IMAGE_ARRAY_KEY = "imagearraykey"
 
 class ImageFragment : Fragment() {
 
+    private lateinit var images: IntArray
+    private lateinit var imageView: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.getIntArray(IMAGE_ARRAY_KEY)?.run{
+            images = this
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        val images = arrayOf(
-            R.drawable.celebrate,
-            R.drawable.caramelapple,
-            R.drawable.chocolatecaramel,
-            R.drawable.godiva
-        )
-
-        return inflater.inflate(R.layout.fragment_image, container, false).apply{
-            findViewById<ImageView>(R.id.imageView).setOnClickListener {
-                (it as ImageView).setImageResource(images.random())
+        return inflater.inflate(R.layout.fragment_image, container, false).apply {
+            imageView = findViewById<ImageView>(R.id.imageView).apply {
+                setOnClickListener { changeImage() }
             }
         }
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        changeImage()
+    }
+
+    fun changeImage() {
+        imageView.setImageResource(images.random())
+    }
+
+    companion object{
+        fun newInstance(images: IntArray) = ImageFragment().apply {
+            arguments = Bundle().apply { putIntArray(IMAGE_ARRAY_KEY, images) }
+        }
     }
 }
--- app/src/main/java/com/example/fragments/MainActivity.kt
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -2,16 +2,39 @@ package com.example.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val dessert_images = intArrayOf(
+            R.drawable.celebrate,
+            R.drawable.caramelapple,
+            R.drawable.chocolatecaramel,
+            R.drawable.godiva
+        )
+
+        val fragment1 = ImageFragment.newInstance(dessert_images)
+
+        val desert_images = intArrayOf(
+            R.drawable.desert_iso,
+            R.drawable.desert_khali,
+            R.drawable.desert_muscat,
+        )
+
+        val fragment2 = ImageFragment.newInstance(desert_images)
+
         supportFragmentManager
             .beginTransaction()
-            .add(R.id.imageFragmentContainer1, ImageFragment())
-            .add(R.id.imageFragmentContainer2, ImageFragment())
+            .add(R.id.imageFragmentContainer1, fragment1)
+            .add(R.id.imageFragmentContainer2, fragment2)
             .commit()
+
+        findViewById<Button>(R.id.changeButton).setOnClickListener {
+            fragment1.changeImage()
+            fragment2.changeImage()
+        }
     }
 }
